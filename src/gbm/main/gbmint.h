1c9749006e0e6e861e34fab04517e32b042d0c76
Simon Ser <contact@emersion.fr>
2020-06-12 17:58:48 +0200

    gbm: add gbm_bo_get_fd_for_plane
    This commit adds a new gbm_bo_get_fd_for_plane function, which does the
    same as gbm_bo_get_fd but allows specifying the plane.
    
    v2: - Rename to gbm_bo_get_fd_for_plane (Emil)
    
    Signed-off-by: Simon Ser <contact@emersion.fr>
    Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
    Reviewed-by: Daniel Stone <daniels@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5442>
    (cherry picked from commit e10a094e0ab96699b9be81d2f5afea5eaa71338b)
    
    Required as drm-tests use this in d91.

--- src/gbm/main/gbmint.h
+++ src/gbm/main/gbmint.h
@@ -83,6 +83,7 @@ struct gbm_device {
    int (*bo_get_fd)(struct gbm_bo *bo);
    int (*bo_get_planes)(struct gbm_bo *bo);
    union gbm_bo_handle (*bo_get_handle)(struct gbm_bo *bo, int plane);
+   int (*bo_get_plane_fd)(struct gbm_bo *bo, int plane);
    uint32_t (*bo_get_height)(struct gbm_bo *bo, int plane);
    uint32_t (*bo_get_stride)(struct gbm_bo *bo, int plane);
    uint32_t (*bo_get_offset)(struct gbm_bo *bo, int plane);
